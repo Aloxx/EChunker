cc68e031f74924a4d393e0aa8d0dbb2a998e301a
Aloxx <105514122+Aloxx@users.noreply.github.com>
2023-02-25 17:26:45 +0300

    Create Errors.js

---
+++ echunker/Errors.js
@@ -0,0 +1,9 @@
+/**
+ * ErrorList for EChunker Library
+ */
+export const ErrorType = {
+   EPOOL_PARAMS: `[EChunker] Object cannot be loaded, uncomfortable ObjectParameters , properties required: x , y , id`,
+   EPOOL_EXISTS: `[EChunker] Object already exists!`,
+   EPOOL_NON_OBJECT: `[EChunker] Object cannot be loaded, argument type is not equals to Object`,
+
+}
